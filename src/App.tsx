96701df699c154167b74dc3b7f1dbeefb8f4fbb0
Luis Alejandro Barranco Juárez <labj86@gmail.com>
2024-07-04 20:57:03 -0600

    uso de contextApi

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
 import CalorieTracker from "./components/CalorieTracker"
+import { useActivity } from "./hooks/useActivity"
 
 function App() {
 
-  const [state, dispatch] = useReducer(activityReducer, initialState)
+  const {state, dispatch} = useActivity()
 
   useEffect(() => {
     localStorage.setItem('activities',JSON.stringify(state.activities))
@@ -34,26 +35,18 @@ function App() {
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form 
-            dispatch={dispatch}
-            state = {state}
-          />
+          <Form />
         </div>
       </section>
 
       <section className="p-10 bg-gray-800">
         <div className="max-w-4xl mx-auto">
-          <CalorieTracker
-            activities={state.activities}
-          />
+          <CalorieTracker />
         </div>
       </section>
 
       <section className="p-10 mx-auto max-w-4xl">
-        <ActivityList 
-          activities={state.activities}
-          dispatch={dispatch}
-        />
+        <ActivityList />
       </section>
     </>
   )
